0681f9722c77a8d1cfd005f461b495da5f45a3ef
DaniDrury <145943483+DaniDrury@users.noreply.github.com>
2023-11-29 20:08:19 -0800

    began form in modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,20 @@
-let date = document.getElementById('dateDiv');
+let date = document.getElementById('date');
+let currentDate;
 
-let currentDate = dayjs().format('MMM D, YYYY h:mm:ss a');
-date.textContent = currentDate;
+//jQuery - loads function once DOM is loaded
+// $(document).ready(function dateTime() {
+//     setInterval(function () {
+//         currentDate = dayjs().format('MMM D, YYYY h:mm:ss a');
+//         date.textContent = currentDate;
+//     }, 1000);
+// });
+
+// Date/Time function that is called first thing on the page
+function dateTime() {
+    setInterval(function () {
+        currentDate = dayjs().format('MMM D, YYYY h:mm:ss a');
+        date.textContent = currentDate;
+    }, 1000);
+}
+
+dateTime();
